2d6dc8905ed431931325575120160860a76804f4
cdchrisDev <juan.feli.lopez@gmail.com>
2024-01-09 02:15:23 -0800

    learning processes

---
+++ docs/3-ForkWaitExec.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(int argc, char *argv[])
+{
+	pid_t MyPid;
+	pid_t getid;
+	int status;
+
+	getid = getpid(); /* tell me who processes */
+	printf("before fork I was one: I identify as %u, and that means chris.\n", getid);
+	MyPid = fork(); /* new process */
+	wait(&status); /* wait until process ends */
+	if (MyPid == -1)
+	{	/* check new process return success*/
+		perror("Error:");
+		return (1);
+	}
+	/* checking process status*/
+	getid = getpid();
+	if (MyPid == 0)
+		printf("After fork I'm another,now %u. I grew up so much I became the child.\n", getid);
+	else
+		printf("I returned to the father, cuz I'm chris again (%u) \n", getid);
+
+	if (MyPid == 0)
+	{
+		static char *newarg[] = { NULL, "hello", "world", NULL};
+		static char *newEnviron[] = { NULL };
+
+		if (argc != 2)
+		{
+			fprintf(stderr, "Usage %s <file-to-exec>\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+		newarg[0] = argv[1];
+
+		execve(argv[1], newarg, newEnviron);
+	}
+	else
+	{
+		printf("I'm your father\n");
+	}
+	return (0);
+
+}
